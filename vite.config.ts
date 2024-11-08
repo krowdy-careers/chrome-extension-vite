135f372522003030601336c4b8adc1dd1df724e4
Anderson Sinche <andersonsinche@MacBook-Pro.local>
2024-11-07 19:07:55 -0500

    [feat] init project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist', // Directorio de salida
+    rollupOptions: {
+      input: {
+        background: 'src/background.ts',
+        content: 'src/content.ts'
+      },
+      output: {
+        // Configura el nombre de los archivos sin hash
+        entryFileNames: '[name].js', // Sin hash en el nombre de archivo
+        chunkFileNames: '[name].js', // Sin hash en el nombre de archivo
+        assetFileNames: '[name].[ext]', // Para otros archivos (como imágenes)
+      },
+    },
+  },
+});
